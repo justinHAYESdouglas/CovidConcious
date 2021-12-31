747ba82d66daa21083a4d206b96dedf35a00fac7
Justin <justin.hayes.douglas@gmail.com>
2021-12-31 00:18:57 -0600

    File structure changes

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const path = require("path");
-const router = express.Router();
-require('dotenv').config();
+// const express = require("express");
+// const path = require("path");
+// const router = express.Router();
+// require('dotenv').config();
 
 
-// const nodemailer = require("nodemailer");
-const cors = require('cors')
+// // const nodemailer = require("nodemailer");
+// const cors = require('cors')
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+// // Define middleware here
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cors());
 
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
-app.use('/', router);
+// app.use('/', router);
 
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+// app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//   });
   
-  app.listen(PORT, function() {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
-  });
+//   app.listen(PORT, function() {
+//     console.log(`🌎 ==> API server now on port ${PORT}!`);
+//   });
