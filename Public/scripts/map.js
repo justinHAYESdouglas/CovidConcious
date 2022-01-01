57264ef20d432a68034c7860186c2b624d00a36a
Justin <justin.hayes.douglas@gmail.com>
2022-01-01 16:14:41 -0600

    Positioned buttons in the shape of the US in maps.js and maps.css

--- Public/scripts/map.js
+++ Public/scripts/map.js
@@ -1,22 +1,21 @@
 $(document).ready(function(){
 
-
-  
   //loop through json file and assing each state a button with an id
   $.getJSON("/scripts/states.json", function(json){
 
-  
-     $.each(json,function (i, json){
-       $("#map-container").append("<button id ="+json.abbr+">"+json.abbr+"</button>")
-     })
-
+    $.each(json,function (i, json){
+      $("#map-container").append("<button id ="+json.abbr+">"+json.abbr+"</button>")
+    });
 
   //position buttons in the shape of the US
-  //TODO make this not terrible   
-  let blank =('<span class = "blank"></span>');
+  let blank = '<span class = "blank"></span>';
   
-  $("#AK").after(blank);
-
+  $("#AK, #ME").after(blank.repeat(10));
+  $("#MI, #FL").after(blank.repeat(2));
+  $("#MA, #RI").after(blank.repeat(1));
+  $("#DE, #GA, #HI").after(blank.repeat(3));
+  $("#DC").after(blank.repeat(6));
+  $("#TX").after(blank.repeat(4));
      
   });
 
