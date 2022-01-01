86353d8f7f2d89f3f62faec767086156077abd12
Justin <justin.hayes.douglas@gmail.com>
2022-01-01 15:17:51 -0600

    Updated position of states to reflect new tile map in discord

--- Public/scripts/map.js
+++ Public/scripts/map.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
+
+
   
+  //loop through json file and assing each state a button with an id
   $.getJSON("/scripts/states.json", function(json){
+
+  
      $.each(json,function (i, json){
        $("#map-container").append("<button id ="+json.abbr+">"+json.abbr+"</button>")
-
      })
+
+
+  //position buttons in the shape of the US
+  //TODO make this not terrible   
+  let blank =('<span class = "blank"></span>');
+  
+  $("#AK").after(blank);
+
+     
   });
+
+  
+  
+ 
+
+
+
+
+
 
   
 });
