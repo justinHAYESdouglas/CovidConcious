103353f58c56015e400a1f5e8c8a5d829ab2b22e
Justin <justin.hayes.douglas@gmail.com>
2022-01-02 18:04:28 -0600

    scope changes

--- Public/scripts/API.js
+++ Public/scripts/API.js
@@ -1,5 +1,5 @@
 // let CovidAPI = process.env.COVIDAPI;
-let CovidInfo = 'https://api.covidactnow.org/v2/states.json?apiKey=' + '';
+let CovidInfo = 'https://api.covidactnow.org/v2/states.json?apiKey=' + '29430bfd9a794c83856c585beafdb31b';
 
 fetch (CovidInfo)
     .then(response =>{
